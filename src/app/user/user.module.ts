d1331d2e8199fb44929308183ca2bd174f33b58a
Rotteror <xeziod@gmail.com>
2021-08-02 20:44:11 +0300

    user Model added

---
+++ src/app/user/user.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
+
+
+@NgModule({
+  declarations: [
+    LoginComponent,
+    RegisterComponent
+  ],
+  imports: [
+    CommonModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  exports: [
+    LoginComponent,
+    RegisterComponent,
+  ]
+
+})
+export class UserModule { }
